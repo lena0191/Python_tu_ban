57e5d781c84e177b30427c6c3e29e3de7cbe7d86
lena0191 <kurokawaakane9669@gmail.com>
2025-07-26 16:32:59 +0700

    Update bai50.py

--- baitaptonghop/bai50.py
+++ baitaptonghop/bai50.py
@@ -1,1 +1,16 @@
-
+n=int(input())
+def xh(n):
+    if(n>=9):
+        return("Xuat sac")
+    elif(n>=8):
+        return("Gioi")
+    elif(n>=6.5):
+        return("Kha")
+    elif(n>=5):
+        return("Trung binh")
+    return("Yeu (em rat nhieu)")
+a=list(map(float,input().split()))
+tong=0
+for i in range (n):
+    tong+=a[i]
+print(f"Diem: {tong/n:.2f}, Xep loai: {xh(tong/n)}")
