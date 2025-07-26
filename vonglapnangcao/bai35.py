d50c899a8b49389db55fbfb8b944b6e1abe69ff8
lena0191 <kurokawaakane9669@gmail.com>
2025-07-26 11:15:57 +0700

    Create bai35.py

---
+++ vonglapnangcao/bai35.py
@@ -0,0 +1,15 @@
+n=int(input())
+def snt(x):
+    if(x==2 or x==3):
+        return 1
+    if(x<2 or x%2==0 or x%3==0):
+        return 0
+    i=5
+    while(i*i<=x):
+        if(x%i==0 or x%(i+2)==0):
+            return 0
+        i+=6
+    return 1
+for i in range (2,n+1):
+    if(snt(i)):
+        print(i,end=' ')
