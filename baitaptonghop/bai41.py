cf306611234e02acaf2ce49b98b069e7284a8edc
lena0191 <kurokawaakane9669@gmail.com>
2025-07-26 12:15:27 +0700

    Create bai41.py

---
+++ baitaptonghop/bai41.py
@@ -0,0 +1,11 @@
+n=float(input())
+if(n>=9):
+    print("Xuat sac")
+elif(n>=8):
+    print("Gioi")
+elif(n>=6.5):
+    print("Kha")
+elif(n>=5):
+    print("Trung binh")
+else:
+    print("Yeu (em rat nhieu)")
